c8f92e7500250408eb029759ffa8ef42578e1727
Mishych <mtcebak@gmail.com>
2024-10-16 16:08:06 +0300

    Added dir with file

---
+++ dir/file.py
@@ -0,0 +1,8 @@
+secretics="jfosnofsnodinfosdnfos"
+
+"-----BEGIN DSA"
+
+
+parol="1939njfjslxnbn2"
+AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
