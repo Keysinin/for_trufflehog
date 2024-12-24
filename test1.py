e0611babb05135154f27a8c5249b1623f5209422
Mishych <mtcebak@gmail.com>
2024-12-24 19:51:10 +0200

    edited test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-github_token = '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'
+github_token = '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'
 
 slack_token = 'xoxp-7442821481331-7440032441317-8203695092662-78a155c4d8fb28624c8c3b4cdf2b2254'
 slack_token_bot = 'xoxb-7442821481331-8203695100758-lkI7KbdkFt27ehz49lWedSRq'
